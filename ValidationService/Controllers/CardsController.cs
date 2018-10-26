22423ed2bd6dadaa821eeb06ee817b54e2361037
Ong Zhixian <zhixian@hotmail.com>
2018-10-26 10:05:43 +0800

    Init commit of CardsController

---
+++ ValidationService/Controllers/CardsController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ValidationService.Models;
+
+namespace ValidationService.Controllers
+{
+    public class CardsController : ApiController
+    {
+        private ValidationServiceContext db = new ValidationServiceContext();
+
+        // GET: api/Cards
+        public IQueryable<Card> GetCards()
+        {
+            return db.Cards;
+        }
+
+        // GET: api/Cards/5
+        [ResponseType(typeof(Card))]
+        public async Task<IHttpActionResult> GetCard(Guid id)
+        {
+            Card card = await db.Cards.FindAsync(id);
+            if (card == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(card);
+        }
+
+        // PUT: api/Cards/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutCard(Guid id, Card card)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != card.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(card).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CardExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Cards
+        [ResponseType(typeof(Card))]
+        public async Task<IHttpActionResult> PostCard(Card card)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Cards.Add(card);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (CardExists(card.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = card.Id }, card);
+        }
+
+        // DELETE: api/Cards/5
+        [ResponseType(typeof(Card))]
+        public async Task<IHttpActionResult> DeleteCard(Guid id)
+        {
+            Card card = await db.Cards.FindAsync(id);
+            if (card == null)
+            {
+                return NotFound();
+            }
+
+            db.Cards.Remove(card);
+            await db.SaveChangesAsync();
+
+            return Ok(card);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CardExists(Guid id)
+        {
+            return db.Cards.Count(e => e.Id == id) > 0;
+        }
+    }
+}
